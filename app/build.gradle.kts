a1d9de1839b72be1220887ada5fbf85235e4b7ca
Carlos-Morales-dev <carlosmorales1937960@gmail.com>
2026-09-23 21:39:23 -0500

    correcion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,19 +18,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("debugConfig") {
-            storeFile = file("${rootDir}/debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
     buildTypes {
-        debug {
-            signingConfig = signingConfigs.getByName("debugConfig")
-        }
         release {
             isMinifyEnabled = false
             proguardFiles(
